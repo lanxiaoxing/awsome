cc71ed17c87ac6ff9de1ebc4017346dfded39e25
leiqiao <leiqiao0829@163.com>
2022-11-11 21:15:33 +0800

    feat:init db

---
+++ src/main/initDb.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("mysql", "root:123456@tcp(localhost:3306)/myoboku")
+	if err != nil {
+		log.Panicln("err:", err.Error())
+	}
+	Db.SetMaxOpenConns(10)
+	Db.SetMaxIdleConns(10)
+}
